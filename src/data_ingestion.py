0f8b37f2e283f71a08fb2715db67c142193a45c3
Ava Dominie <avadominie@gmail.com>
2025-09-03 21:23:10 +0000

    added logging

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,7 +1,25 @@
-
 import os
+import logging
 from dotenv import load_dotenv
 
 
 
 load_dotenv()
+
+
+# Logging setup
+VSCODE_WORKSPACE_FOLDER = os.getenv('VSCODE_WORKSPACE_FOLDER', os.getcwd())
+LOG_DIR = os.path.join(VSCODE_WORKSPACE_FOLDER, 'src/logs')
+os.makedirs(LOG_DIR, exist_ok=True)
+LOG_FILE = os.path.join(LOG_DIR, 'data_ingestion.log')
+
+logging.basicConfig(
+	level=logging.INFO,
+	format='%(asctime)s - %(levelname)s - %(message)s',
+	handlers=[
+		logging.FileHandler(LOG_FILE),
+		logging.StreamHandler()
+	]
+)
+logger = logging.getLogger("data_ingestion")
+logger.info("Logger initialized successfully")
